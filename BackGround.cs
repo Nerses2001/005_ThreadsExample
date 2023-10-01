e733626f87d9ab5f7899a6a8b7f31d14acb633c8
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-01 03:59:48 -0800

    Backgraund = false

---
+++ BackGround.cs
@@ -0,0 +1,17 @@
+﻿namespace _005_ThreadsExample
+{
+    internal class BackGround
+    {
+        public static void Function()
+        {
+            for (int i = 0; i < 500; ++i)
+            {
+                Thread.Sleep(10);
+                Console.Write(".");
+            }
+            Console.WriteLine("\nSecondery Finished");
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using _005_ThreadsExample;
+
+var thread = new Thread(BackGround.Function);
+
+thread.IsBackground = false;
+
+thread.Start();
+Thread.Sleep(500);
+
+Console.WriteLine("\n Primary finished");
